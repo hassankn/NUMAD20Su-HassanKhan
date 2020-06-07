0d5d14adf6bfb87b1180dea1e1ac75f6d4ca2c83
Hassan <40873451+hassankn@users.noreply.github.com>
2020-06-07 15:25:09 -0400

    location tracket fragment and layout

---
+++ app/src/main/java/edu/neu/madcourse/numad20su_hassankhan/LocatorFragment.java
@@ -0,0 +1,54 @@
+package edu.neu.madcourse.numad20su_hassankhan;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.sql.SQLOutput;
+
+public class LocatorFragment extends Fragment {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private TextView location_coordinates;
+    private String locationCoordinatesString;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_locator, container, false);
+    }
+
+    public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this.getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            locationCoordinatesString = location.getLatitude() + ", " + location.getLongitude();
+
+                            location_coordinates = view.findViewById(R.id.location_coordinates);
+                            location_coordinates.setText(locationCoordinatesString);
+                        }
+                    }
+                });
+    }
+}
